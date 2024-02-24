0432ee598e864f74645eddb201ab2035c5881022
BelanovaMartinaTrain <belanova.m@gmail.com>
2024-02-24 12:08:53 +0100

    changed progress bars and measures to avoid layout shift

--- app/components/Clock.tsx
+++ app/components/Clock.tsx
@@ -11,24 +11,45 @@ type Props = {
 
 export default function Clock(props: Props) {
     const [time, setTime] = useState(new Date(props.time));
-    const [timezoneAbbreviation, setTimezoneAbbreviation] = useState(props.tmzAbrr);
+    const [timezoneAbbreviation, setTimezoneAbbreviation] = useState(
+        props.tmzAbrr
+    );
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const timer = setInterval(() => {
             setTime(new Date());
+            setIsLoading(false);
         }, 1000);
-        setTimezoneAbbreviation(Intl.DateTimeFormat().resolvedOptions().timeZone);
+        setTimezoneAbbreviation(
+            Intl.DateTimeFormat().resolvedOptions().timeZone
+        );
         return () => clearInterval(timer);
     }, []);
 
     return props.timezone ? (
-        <h2 suppressHydrationWarning>
-            {time.toLocaleTimeString([], { timeZone: `${props.timezone}`, hourCycle: "h23", hour: "2-digit", minute: "2-digit" })}
+        <h2
+            suppressHydrationWarning
+            className={`${!!isLoading && "visibility-hidden"}`}
+        >
+            {time.toLocaleTimeString([], {
+                timeZone: `${props.timezone}`,
+                hourCycle: "h23",
+                hour: "2-digit",
+                minute: "2-digit",
+            })}
             {" UTC"}
         </h2>
     ) : (
-        <h2 suppressHydrationWarning>
-            {time.toLocaleTimeString([], { hourCycle: "h23", hour: "2-digit", minute: "2-digit" })}
+        <h2
+            suppressHydrationWarning
+            className={`${!!isLoading && "visibility-hidden"}`}
+        >
+            {time.toLocaleTimeString([], {
+                hourCycle: "h23",
+                hour: "2-digit",
+                minute: "2-digit",
+            })}
             {` ${timezoneAbbreviation}`}
         </h2>
     );
