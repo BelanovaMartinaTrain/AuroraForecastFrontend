dc2ced0451052a433c41fb110c2d106befe69b71
BelanovaMartinaTrain <belanova.m@gmail.com>
2024-02-26 17:40:01 +0100

    menu font size

--- app/components/mainComponents/WidgetAuroraActivity.tsx
+++ app/components/mainComponents/WidgetAuroraActivity.tsx
@@ -23,7 +23,7 @@ export default function WidgetAuroraActivity() {
                     isOpen={isOpen}
                     onOpenChange={onOpenChange}
                     isDismissable={true}
-                    className=" bg-black bg-opacity-75 backdrop-blur-md rounded-xl "
+                    className=" bg-black bg-opacity-75 backdrop-blur-sm rounded-xl "
                     disableAnimation={true}
                     scrollBehavior="inside"
                 >
