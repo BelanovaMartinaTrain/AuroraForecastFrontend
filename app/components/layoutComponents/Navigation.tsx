58dc3fd761e40e6caa3a579e866974d444e4b6ba
BelanovaMartinaTrain <belanova.m@gmail.com>
2024-02-27 23:15:17 +0100

    added link highlight

--- app/components/layoutComponents/Navigation.tsx
+++ app/components/layoutComponents/Navigation.tsx
@@ -5,19 +5,20 @@ import { usePathname } from "next/navigation";
 
 export default function Navigation({ cssClass, linksClass }: { cssClass?: string; linksClass?: string }) {
     const pathname = usePathname();
+    console.log(pathname);
     return (
         <nav className={cssClass} role="menu">
             <Link href="/" role="menu item" aria-label="Navigate to Dashboard ">
-                <h2 className={`${linksClass} ${pathname === "/" ? "active font-[600] " : ""} hover:font-bold`}>DASHBOARD</h2>
+                <h2 className={`${linksClass} ${pathname === "/" ? "active font-semibold " : ""} hover:font-bold`}>DASHBOARD</h2>
             </Link>
             <Link href="/graphs" role="menu item" aria-label="Navigate to Graphs ">
-                <h2 className={`${linksClass} ${pathname === "/graphs" ? "active font-[600]" : ""} hover:font-bold`}>GRAPHS</h2>
+                <h2 className={`${linksClass} ${pathname === "/graphs" ? "active font-semibold " : ""} hover:font-bold`}>GRAPHS</h2>
             </Link>
             <Link href="/weather" role="menu item" aria-label="Navigate to Weather ">
-                <h2 className={`${linksClass} ${pathname === "/weather" ? "active font-[600]" : ""} hover:font-bold`}>WEATHER</h2>
+                <h2 className={`${linksClass} ${pathname === "/weather" ? "active font-semibold " : ""} hover:font-bold`}>WEATHER</h2>
             </Link>
             <Link href="/about" role="menu item" aria-label="Navigate to About section">
-                <h2 className={`${linksClass} ${pathname === "/about" ? "active font-[600]" : ""} hover:font-bold`}>ABOUT</h2>
+                <h2 className={`${linksClass} ${pathname === "/about" ? "active font-semibold " : ""} hover:font-bold`}>ABOUT</h2>
             </Link>
         </nav>
     );
