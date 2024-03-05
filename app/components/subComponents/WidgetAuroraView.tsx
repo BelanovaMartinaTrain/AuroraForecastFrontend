4c3f2e060d9cc38c424a5b202fab1b9631f61bd9
BelanovaMartinaTrain <belanova.m@gmail.com>
2024-03-05 22:24:16 +0100

    little details

--- app/components/subComponents/WidgetAuroraView.tsx
+++ app/components/subComponents/WidgetAuroraView.tsx
@@ -17,7 +17,7 @@ export default function WidgetAuroraView() {
             setIsLoading(true);
             setError("");
             try {
-                const kpData = await fetchData("http://209.38.184.216:8080/api/planetary-k-index-mod");
+                const kpData = await fetchData("http://165.227.128.185:8080/api/planetary-k-index-mod");
                 const stormData = await fetchData("http://209.38.184.216:8080/api/sunstorm-events");
 
                 if (!!kpData.cause || !!stormData.cause) {
