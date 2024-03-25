abdc69df2d7f33bf111f3cd7736d995125a17c84
BelanovaMartinaTrain <belanova.m@gmail.com>
2024-03-25 23:02:18 +0100

    changed wether data fetching small refactorings

--- app/_components/subComponents/WeatherTable.tsx
+++ app/_components/subComponents/WeatherTable.tsx
@@ -13,7 +13,10 @@ export default function WeatherTable() {
     return (
         <>
             <div className="mb-8 ml-4">
-                <div key={-1} className="flex justify-evenly pt-1 pb-2 border-b-[1px] border-b-black border-opacity-50">
+                <div
+                    key={-1}
+                    className="flex justify-evenly pt-1 pb-2 border-b-[1px] border-b-black border-opacity-50"
+                >
                     <p className="capitalize font-medium min-w-28">Date and time</p>
                     <div className="capitalize font-medium flex w-full ">
                         <p className={`${PClassNames}`}>Temperature </p>{" "}
@@ -28,12 +31,18 @@ export default function WeatherTable() {
                 {!!weatherArray ? (
                     weatherArray?.map((weather, index) => {
                         return (
-                            <div key={index} className="flex justify-evenly py-1 border-b-[1px] border-b-black border-opacity-50">
+                            <div
+                                key={index}
+                                className="flex justify-evenly py-1 border-b-[1px] border-b-black border-opacity-50"
+                            >
                                 <p className=" font-medium min-w-28">
-                                    {new Date(weather.time).toLocaleDateString(`${units === "C" ? "uk" : "us"}`, {
-                                        month: "numeric",
-                                        day: "numeric",
-                                    })}
+                                    {new Date(weather.time).toLocaleDateString(
+                                        `${units === "C" ? "uk" : "us"}`,
+                                        {
+                                            month: "numeric",
+                                            day: "numeric",
+                                        }
+                                    )}
                                     {`${units === "C" ? "." : ""}`}
                                     <span className="pl-2">{`    `}</span>
                                     {new Date(weather.time).toLocaleTimeString([], {
@@ -47,26 +56,50 @@ export default function WeatherTable() {
                                         {" "}
                                         {units === "C"
                                             ? Math.round(weather.air_temperature)
-                                            : Math.round(weather.air_temperature * (9 / 5) + 32)}{" "}
+                                            : Math.round(
+                                                  weather.air_temperature * (9 / 5) + 32
+                                              )}{" "}
                                         &#176;{units}
                                     </p>{" "}
                                     <img
                                         className=" "
-                                        src={`icons/weather/${weatherSymbolKeys[weather.icon_code as TWeatherSymbolKey]}.svg`}
+                                        src={`icons/weather/${
+                                            weatherSymbolKeys[
+                                                weather.icon_code as TWeatherSymbolKey
+                                            ]
+                                        }.svg`}
                                         width="20px relaive top-0"
-                                        alt={weatherAlt[weatherSymbolKeys[weather.icon_code as TWeatherSymbolKey].slice(0, 2) as TWeatherAltKey]}
+                                        alt={
+                                            weatherAlt[
+                                                weatherSymbolKeys[
+                                                    weather.icon_code as TWeatherSymbolKey
+                                                ].slice(0, 2) as TWeatherAltKey
+                                            ]
+                                        }
                                     />
                                 </div>
 
                                 <p className="font-medium w-full lowercase">
                                     {" "}
-                                    {units === "C" ? `${Math.round(weather.wind_speed)} m/s` : `${Math.round(weather.wind_speed * 2.2369)} mph`}
+                                    {units === "C"
+                                        ? `${Math.round(weather.wind_speed)} m/s`
+                                        : `${Math.round(weather.wind_speed * 2.2369)} mph`}
                                 </p>
-                                <p className={PClassNames}>{Math.round(weather.cloud_area_fraction)} %</p>
-                                <p className={`${PClassNames} hidden md:block`}>{Math.round(weather.cloud_area_fraction_high)} %</p>
-                                <p className={`${PClassNames} hidden md:block`}>{Math.round(weather.cloud_area_fraction_medium)} %</p>
-                                <p className={`${PClassNames} hidden md:block`}>{Math.round(weather.cloud_area_fraction_low)} %</p>
-                                <p className={`${PClassNames} hidden lg:block`}>{Math.round(weather.fog_area_fraction)} %</p>
+                                <p className={PClassNames}>
+                                    {Math.round(weather.cloud_area_fraction)} %
+                                </p>
+                                <p className={`${PClassNames} hidden md:block`}>
+                                    {Math.round(weather.cloud_area_fraction_high)} %
+                                </p>
+                                <p className={`${PClassNames} hidden md:block`}>
+                                    {Math.round(weather.cloud_area_fraction_medium)} %
+                                </p>
+                                <p className={`${PClassNames} hidden md:block`}>
+                                    {Math.round(weather.cloud_area_fraction_low)} %
+                                </p>
+                                <p className={`${PClassNames} hidden lg:block`}>
+                                    {Math.round(weather.fog_area_fraction)} %
+                                </p>
                             </div>
                         );
                     })
@@ -76,11 +109,21 @@ export default function WeatherTable() {
             </div>
             <div className="flex justify-evenly mt-5">
                 <p className=" font-medium text-stone-500 text-[11px]  mr-2">
-                    Used location: {lat}, {lon}
+                    <Link
+                        href={`https://www.google.com/maps/place/${lat},${lon}`}
+                        target="_blank"
+                        aria-label="Navigate to google maps to see used location on a map"
+                    >
+                        Used location: {lat}, {lon}
+                    </Link>
                 </p>
                 <p className=" font-medium text-stone-500 text-[11px]">
                     <span className="capitalize mr-1">Source:</span>
-                    <Link href="https://www.yr.no/en" target="_blank" aria-label="Link to source of used data - Norway meteo institute">
+                    <Link
+                        href="https://www.yr.no/en"
+                        target="_blank"
+                        aria-label="Link to source of used data - Norway meteo institute"
+                    >
                         MET Norway
                     </Link>
                 </p>
